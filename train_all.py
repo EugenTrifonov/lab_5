aa5d612e88ae827aff3059a3db8698ea94b517e0
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-04-02 23:40:46 +0300

    Update train_all.py

--- train_all.py
+++ train_all.py
@@ -119,7 +119,7 @@ def main():
   )
   unfreeze_model(model)
   model.compile(
-    optimizer=tf.optimizers.Adam(0.000001),
+    optimizer=tf.optimizers.Adam(0.00000001),
     loss=tf.keras.losses.categorical_crossentropy,
     metrics=[tf.keras.metrics.categorical_accuracy],
   )
