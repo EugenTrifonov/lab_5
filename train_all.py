240cd627d1cdeb2867c6b6dff350dae51ebf2435
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-04-04 18:59:26 +0300

    Update train_all.py

--- train_all.py
+++ train_all.py
@@ -112,7 +112,7 @@ def main():
   log_dir='{}/owl-{}'.format(LOG_DIR, time.time())
   model.fit(
     train_dataset,
-    epochs=20,
+    epochs=15,
     validation_data=validation_dataset,
     callbacks=[
       tf.keras.callbacks.TensorBoard(log_dir),lrate
